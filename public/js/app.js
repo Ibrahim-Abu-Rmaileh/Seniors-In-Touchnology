a0441cf52ac89f57c206e6d2c0006612464eca20
Afek H <afek590@gmail.com>
2016-04-18 18:05:55 +0300

    Minor changes in google api

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,20 @@ var MANAGER;
 }());*/
 
 (function(){
-    var app = angular.module('SIT', []);
+    var app = angular.module('SIT', ['ngRoute']);
+    
+    app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when('/',{
+        controller: 'MainController',
+        controllerAs: 'main',
+        templateUrl: 'views/main.html'})
+    .when('/panel',{
+        controller: 'MainController',
+        controllerAs: 'main',
+        templateUrl: 'views/panel.html'})
+          });
+    }]);
 
     app.controller('MainController', ['$scope', '$window', function($scope, $window) {
         MANAGAR = false;
