f8a90a02a9281c960b685934d410c8bf19cf1c90
Tal-C <tal3433@gmail.com>
2016-05-28 19:27:56 +0300

    creation a mail requests platform

--- public/javascripts/courseRegScript.js
+++ public/javascripts/courseRegScript.js
@@ -2,10 +2,18 @@ angular.module('courseReg', [])
     .controller('CoureRegController', ['$scope', '$http', function($scope, $http){
         $scope.courseRegList;
 
-        $scope.loadCourseReg = function(){
+        $scope.clickListener = function(e)
+        {
+            if(e.target.id == "sendEmail")
+                emailjs.send("gmail","gmail",{name:$("#username").html(), email:$("#email").html(), tel:$("#tel").html()});
+        }
+
+        $scope.loadCourseReg = function()
+        {
             $http.get('/course')
                 .success(function(res){
                     $scope.courseRegList = res;
+                    $("#sendEmail").click(clickListener);
                 })
                 .catch(function(err) {
                     console.log('Get courses error.');
