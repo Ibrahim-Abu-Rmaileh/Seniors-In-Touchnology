255d1a3fa02bee91822de96ccece9fe6bd82a773
DorianTs <doriants@gmail.com>
2016-05-28 20:15:48 +0300

    minor changes

--- public/javascripts/courseRegScript.js
+++ public/javascripts/courseRegScript.js
@@ -2,10 +2,17 @@ angular.module('courseReg', [])
     .controller('CoureRegController', ['$scope', '$http', function($scope, $http){
         $scope.courseRegList;
 
+        /*Listener to button clicks*/
         var clickListener = function(e)
         {
+            /*If send email button was clicked, use the API of emailJS to send
+            * email to a specific email we configured before.
+            * email sended to - sitjce@gmail.com*/
 
-
+            /*
+            * TO_DO :
+            * 1)send also the wanted courses as part of the email
+            * 2)clone all this logic to the volunteer page*/
             if(e.target.id == "sendEmail")
             {
                 emailjs.send("gmail","gmail",{name:$("#username").val(), email:$("#email").val(), tel:$("#tel").val()}).then(
@@ -17,21 +24,6 @@ angular.module('courseReg', [])
                     }
                 );
             }
-
-           if(e.target.id == "sendEmail")
-           {
-               emailjs.send("gmail","gmail",{name:$("#username").val(), email:$("#email").val(), tel:$("#tel").val()}).then(
-                   function(response) {
-                       alert("המייל נשלח בהצלחה");
-                   },
-                   function(error) {
-                       alert("נכשל");
-                   }
-               );
-           }
-
-
-
         }
 
         $scope.loadCourseReg = function()
