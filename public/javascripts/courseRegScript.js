faff10bf3299cfc7e0056f3cdd3e671eef900f22
Tal-C <tal3433@gmail.com>
2016-06-02 15:16:34 +0300

    Added email.js sending email platform to volunteering page

--- public/javascripts/courseRegScript.js
+++ public/javascripts/courseRegScript.js
@@ -9,15 +9,12 @@ angular.module('courseReg', [])
             * email to a specific email we configured before.
             * email sended to - sitjce@gmail.com*/
 
-            /*
-            * TO_DO :
-            * 1)send also the wanted courses as part of the email
-            * 2)clone all this logic to the volunteer page*/
             if(e.target.id == "sendEmail")
             {
                 /*First, we need to get list of courses that the user chose
                 * in the checkboxes*/
                 var i;
+                /* getting an array of checkboxes from the html*/
                 var checkboxArray = $('.checkbox');
                 var chosenCourses = new Array(); // filling this string array with the names
                 for(i=0; i<$scope.courseRegList.length; i++)
@@ -28,7 +25,7 @@ angular.module('courseReg', [])
                     }
                 }
 
-                emailjs.send("gmail","gmail",{
+                emailjs.send("gmail","regCourse",{
                     name:$("#username").val(),
                     email:$("#email").val(),
                     tel:$("#tel").val(),
--- public/javascripts/volunteersRegScript.js
+++ public/javascripts/volunteersRegScript.js
@@ -2,6 +2,46 @@ angular.module('volunteersReg', [])
     .controller('volunteersRegController', ['$scope', '$http', function($scope, $http){
         $scope.volunteersRegList;
 
+        /*Listener to button clicks*/
+        var clickListener = function(e)
+        {
+            /*If send email button was clicked, use the API of emailJS to send
+             * email to a specific email we configured before.
+             * email sended to - sitjce@gmail.com*/
+
+            if(e.target.id == "sendEmail")
+            {
+                /*First, we need to get list of volunteer exhibitions that the user chose
+                 * in the checkboxes*/
+                var i;
+                /* getting an array of checkboxes from the html*/
+                var checkboxArray = $('.checkbox');
+                var chosenVols = new Array(); // filling this string array with the names
+                for(i=0; i<$scope.volunteersRegList.length; i++)
+                {
+                    if(checkboxArray[i].checked == true) // if checkbox set to true
+                    {
+                        chosenVols.push(checkboxArray[i].defaultValue); // save course name
+                    }
+                }
+
+                emailjs.send("gmail","regVol",{
+                    name:$("#username").val(),
+                    email:$("#email").val(),
+                    tel:$("#tel").val(),
+                    vol:chosenVols,
+                    notes:$("#notes").val()
+                }).then(
+                    function(response) {
+                        alert("המייל נשלח בהצלחה");
+                    },
+                    function(error) {
+                        alert("נכשל");
+                    }
+                );
+            }
+        }
+
         $scope.loadvolunteersReg = function(){
             $http.get('/getvolunteers')
                 .success(function(res){
